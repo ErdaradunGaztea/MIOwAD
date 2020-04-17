98f83ae6e4a4b7ffb8facf0366cb9d8795da4081
Mateusz <matibakala@gmail.com>
2020-04-17 17:58:47 +0200

    updated 1.2.6 with no regularization comparison

--- main_6.py
+++ main_6.py
@@ -12,6 +12,12 @@ train = pandas.read_csv('resources/regression/multimodal-sparse-training.csv', i
 x_train = train.iloc[:, :-1]
 y_train = train.iloc[:, -1:]
 
+nn0 = NeuralNet(1, normal_init)\
+    .add_layer(Layer(10, sigmoid))\
+    .add_layer(Layer(5, sigmoid))\
+    .add_layer(Layer(1, linear))
+nn0.budget.set_epoch_limit(50).set_detection_limit(1.5)
+
 nn1 = NeuralNet(1, normal_init)\
     .add_layer(Layer(10, sigmoid))\
     .add_layer(Layer(5, sigmoid))\
@@ -26,10 +32,12 @@ nn2 = NeuralNet(1, normal_init)\
     .set_regularization(L2_regularization.set_params({"coef": 0.05}))
 nn2.budget.set_epoch_limit(50).set_detection_limit(1.5)
 
+nn0.train(np.transpose(x_train), np.transpose(y_train), learning_rate=0.005, batch_size=8)
 nn1.train(np.transpose(x_train), np.transpose(y_train), learning_rate=0.005, batch_size=8)
 nn2.train(np.transpose(x_train), np.transpose(y_train), learning_rate=0.005, batch_size=8)
 
 # plot
+plt.plot(np.arange(len(nn0.loss_history)), nn0.loss_history, label="none")
 plt.plot(np.arange(len(nn1.loss_history)), nn1.loss_history, label="L1")
 plt.plot(np.arange(len(nn2.loss_history)), nn2.loss_history, label="L2")
 plt.xlabel("epoch")
